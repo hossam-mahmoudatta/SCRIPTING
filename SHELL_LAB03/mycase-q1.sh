3330c8459ea881c3f2fc8e43500ff74f2836bb04
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-12-21 14:16:15 +0200

    editing in q1

--- SHELL_LAB03/mycase-q1.sh
+++ SHELL_LAB03/mycase-q1.sh
@@ -1,72 +1,28 @@
 #!/usr/bin/zsh
+export LC_COLLATE=C;
 
 # Will copy now without any arguments
-listDIRX() {
-    if [ $# -lt 1 ];
-    then
-        ls # Moves to the home DIR
-        echo "Current DIR is listed"
-    elif [ $# -eq 1 ];
-    then
-        case "$1" in
-            -l)
-                ls -l
-                echo "\nCurrent DIR longlist is listed"
-                ;;
-            -a)
-                ls -a
-                echo "\nCurrent DIR hidden is listed"
-                ;;
-            -d)
-                ls -d
-                echo "\nCurrent DIR dir is listed"
-                ;;
-            -i)
-                ls -i
-                echo "\nCurrent DIR inode"
-                ;;
-            -R)
-                ls -R
-                echo "\nCurrent DIR recursive"
-                ;;
-            *)
-                dir="$1"
-                ls -l "$dir"
-                echo "You listed $dir !"
-                ;;
-        esac
+mycase() {
+    # Ask for the user's name
+    print "Hello!\nWelcome to Hosa's Shell!\n"
+    print "Please enter the character you want to check:"
 
-    elif [ $# -eq 2 ];
-    then
-        dir="$2"
-        case "$1" in
-            -l)
-                ls -l "$dir"
-                echo "\nCurrent DIR longlist is listed"
-                ;;
-            -a)
-                ls -a "$dir"
-                echo "\nCurrent DIR hidden is listed"
-                ;;
-            -d)
-                ls -d "$dir"
-                echo "\nCurrent DIR dir is listed"
-                ;;
-            -i)
-                ls -i "$dir"
-                echo "\nCurrent DIR inode"
-                ;;
-            -R)
-                ls -R "$dir"
-                echo "\nCurrent DIR recursive"
-                ;;
-            *)
-                dir="$1"
-                ls -l "$dir"
-                echo "You listed $dir !"
-                ;;
-        esac
-    else
-        echo "Usage: listDIR [DIR]"
-    fi
+    read inputChar
+
+    print "\nLets check your input now!\n"
+    
+    case "$inputChar" in
+        +([a-z]))
+            echo "\nInputted character is a lowercase letter."
+            ;;
+        +([A-Z]))
+            echo "\nInputted character is an uppercase letter."
+            ;;
+        +([0-9]))
+            echo "\nInputted character is a number."
+            ;;
+        *)
+            echo "Nothing was entered"
+            ;;
+    esac
 }
