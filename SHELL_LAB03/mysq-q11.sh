8e1ddbfd6ff3f9b551a7586ddf882614882a0b64
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-12-21 18:07:42 +0200

    created q11

---
+++ SHELL_LAB03/mysq-q11.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/zsh
+
+# This is a menu
+while true;
+do
+    select choice in "ls" "ls -a" "exit"
+    do
+        case $choice in
+            "ls")
+                ls
+
+                ;;
+            "ls -a")
+                ls -a
+                ;;
+            "exit")
+                print "Program will exit now"
+                # exit 0
+                break 2
+                ;;
+            *)
+                print "No Valid Entry"
+                ;;
+        esac
+    done
+done
