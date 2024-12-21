23981a0b92854230683c23081f660a9481d7ae91
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-12-21 16:13:23 +0200

    Updated q4

--- SHELL_LAB03/mybackup-q4.sh
+++ SHELL_LAB03/mybackup-q4.sh
@@ -12,9 +12,15 @@ mybackup() {
   # Loop through all files and directories in /home/hosa
   for file in /home/hosa/*;
   do
-    backupFiles="$backupFiles $file"
+    # This will check if its a file or a dir
+    if [ -f $file ];
+    then
+      # this will append files into the overall file
+      backupFiles="$backupFiles $file"
+    fi
   done
 
+  # Here happens the backup and compression
   sudo tar -czvf "$hosaBackupFile" -C /home $backupFiles
   
   # Verify the backup file
