a08cd1bc0117421370ec09a4525e92b33332fa9b
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-12-22 17:44:33 +0200

    now starting q6

--- SHELL_LAB03/chkmail-q6.sh
+++ SHELL_LAB03/chkmail-q6.sh
@@ -1,6 +1,37 @@
-#!/usr/bin/zsh
+#!/bin/bash
 
-# Will copy now without any arguments
-mymail() {
+chkmail() {
+  # A test to check if the mail template exists
+  if [ ! -f "mtemplate" ];
+  then
+    echo "Mail template file 'mtemplate' not found!"
+    exit 1
+  fi
+
+  # Get the hostname
+  hostname=$(hostname)
+
+  # Read mail body from the file
+  mailBody=$(cat mtemplate)
+
+  # Get a list of all valid users
+  users=$(cut -d: -f1 /etc/passwd)
+
+  # Define the email subject
+  subject="System Wide Notification from Hosa"
+
+  # Loop through each user and send the mail
+  for user in $users;
+  do
+    # Check if the user has a valid home directory (to exclude system accounts)
+    # home_dir=$(getent passwd "$user" | cut -d: -f6)
     
+    homeDIR=$(awk -F: -v user="$user" '$1 == user {print $6}' /etc/passwd)
+
+    if [ -d "$homeDIR" ]; then
+      email="${user}@${hostname}"
+      echo "$mailBody" | sudo mail -s "$subject" "$email"
+      echo "Mail sent to $email"
+    fi
+  done
 }
--- SHELL_LAB03/mymail-q5.sh
+++ SHELL_LAB03/mymail-q5.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
-# Check if the mail template exists
-if [ ! -f "mtemplate" ]; then
-  echo "Mail template file 'mtemplate' not found!"
-  exit 1
-fi
-
-# Get the hostname
-hostname=$(hostname)
-
-# Read mail body from the file
-mail_body=$(cat mtemplate)
-
-# Get a list of all valid users
-users=$(cut -d: -f1 /etc/passwd)
-
-# Define the email subject
-subject="System Notification"
-
-# Loop through each user and send the mail
-for user in $users; do
-  # Check if the user has a valid home directory (to exclude system accounts)
-  home_dir=$(getent passwd "$user" | cut -d: -f6)
-  if [ -d "$home_dir" ]; then
-    email="${user}@${hostname}"
-    echo "$mail_body" | mail -s "$subject" "$email"
-    echo "Mail sent to $email"
+mymail() {
+  # A test to check if the mail template exists
+  if [ ! -f "mtemplate" ];
+  then
+    echo "Mail template file 'mtemplate' not found!"
+    exit 1
   fi
-done
+
+  # Get the hostname
+  hostname=$(hostname)
+
+  # Read mail body from the file
+  mailBody=$(cat mtemplate)
+
+  # Get a list of all valid users
+  users=$(cut -d: -f1 /etc/passwd)
+
+  # Define the email subject
+  subject="System Wide Notification from Hosa"
+
+  # Loop through each user and send the mail
+  for user in $users;
+  do
+    # Check if the user has a valid home directory (to exclude system accounts)
+    # home_dir=$(getent passwd "$user" | cut -d: -f6)
+    
+    homeDIR=$(awk -F: -v user="$user" '$1 == user {print $6}' /etc/passwd)
+
+    if [ -d "$homeDIR" ]; then
+      email="${user}@${hostname}"
+      echo "$mailBody" | sudo mail -s "$subject" "$email"
+      echo "Mail sent to $email"
+    fi
+  done
+}
