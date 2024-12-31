6446d07b17231b97e47bbf8a991df98e01b024b6
hossam-mahmoudatta <hossammaali@gmail.com>
2024-12-31 18:37:34 +0200

    Finished the lab testing

---
+++ PYTHON_LAB01/NOTEBOOK_FILES/intro.py
@@ -0,0 +1,59 @@
+# 1- define a variable
+
+name = "Mina"
+
+std= "Abdelrahman"
+
+import sys
+
+print(sys.getsizeof(name))
+
+empty  = ""
+print(sys.getsizeof(empty))
+
+num = 10
+
+happy = True # False
+
+g  =9.8
+print(g)
+
+
+
+# this  a comment
+
+
+name = "Abbass "
+
+if name=='ahmed' :
+    print("Happy")
+
+print("-------------------naming ----------------------")
+
+# if = "10"
+
+num = 100
+print(num)
+
+# print= "iti"
+# print('abbass')
+
+# str = 'iti'
+
+# bio = "My name is noha "; email="noha@iti.com"
+
+# bio=("My name is noha, \n"
+#      "I works at iti "
+#      "I lives in Mansoura")
+#
+# print(bio)
+
+bio = """My name is noha
+I works at ITI
+I lives in Mansorua"""
+print(bio)
+
+
+"we can use this string as a comment "
+
+
---
+++ PYTHON_LAB01/NOTEBOOK_FILES/playground.py
@@ -0,0 +1,45 @@
+
+
+
+# l = ["Menna", "iti", 10, True, "Menna"]
+
+# for item in l:
+#     print(f'{item}: {l.index(item)}')
+
+# count = 0
+# for item in l:
+#     print(f'{item}: {count}')
+#     count += 1
+
+
+# I need to create an iterator for the list ??
+# enumarate
+# lenum= enumerate(l)
+# print(lenum) # enumerate object  # iterable
+
+# cast to list
+# list_of_items = list(lenum)
+# print(list_of_items)
+# for index, item in lenum:
+#     print(index, item)
+#
+# for index, item in lenum:
+#     print(f"here {index}, {item}")
+
+#######################################
+
+
+
+
+
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
+
+
+
+
+
+
