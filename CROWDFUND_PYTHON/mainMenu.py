795cb60cb0544427d49d782b6880b8d6d2a4a723
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-01-04 14:31:27 +0200

    Finalized the main menu, and will import functions to it

--- CROWDFUND_PYTHON/mainMenu.py
+++ CROWDFUND_PYTHON/mainMenu.py
@@ -1,3 +1,5 @@
+# Hossam Mahmoud
+# Creating a Crowd Funding Application
 
 def welcomeMsg():
     print("\nWelcome to Hosa Crowd Funding App:")
@@ -13,17 +15,38 @@ def mainMenu():
     while True:
         showMainMenu()
         choice = input("Enter your choice: ")
-        
-        if choice == 1:
-            print("You'll sign up now.")
-        elif choice == 2:
-            print("You can sign in now.")
-        elif choice == 3:
-            print("You'll exit now.")
+        if choice == '1':
+            print("\nYou'll sign up now.")
+        elif choice == '2':
+            print("\nYou can sign in now.")
+        elif choice == '3':
+            print("\nYou'll exit now.")
             break
         else:
-            print("Invalid Choice")
-
+            print("\nInvalid Choice")
+                
+        # if choice.isdigit():
+        #     choice = int(choice)
 # Run the program
 welcomeMsg()
 mainMenu()
+
+
+
+
+
+
+
+
+
+
+            # match choice:
+            #     case 1:
+            #         print("You'll sign up now.")
+            #     case 2:
+            #         print("You can sign in now.")
+            #     case 3:
+            #         print("You'll exit now.")
+            #         break
+            #     case _:
+            #         print("Invalid Choice")
