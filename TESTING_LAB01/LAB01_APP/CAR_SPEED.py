f12024a2581c699fad6a74fad89e167387c617d4
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-01-15 17:58:00 +0200

    Creating the 1st task files

---
+++ TESTING_LAB01/LAB01_APP/CAR_SPEED.py
@@ -0,0 +1,12 @@
+# ==============================================
+# Created By: Hossam Mahmoud
+# Date: 2025-01-15
+# Filename: CAR_SPEED.py
+# Description: Brief description of what the file does
+# Version: 1.0.0
+# Last Modified: 2025-01-15 by Hossam
+# Dependencies: requests, json
+# Copyright (c) 2025 Hossam. All rights reserved.
+# TODO: Add future tasks or improvements
+# ==============================================
+
---
+++ TESTING_LAB01/LAB01_APP/CAR_SPEED_TESTER.py
@@ -0,0 +1,16 @@
+# ==============================================
+# Created By: Hossam Mahmoud
+# Date: 2025-01-15
+# Filename: CAR_SPEED_TESTER.py
+# Description: Test script for testing the Car Speed Function
+# Version: 1.0.0
+# Last Modified: 2025-01-15 by Hossam
+# Dependencies: requests, json
+# Copyright (c) 2025 Hossam. All rights reserved.
+# TODO: Add future tasks or improvements
+# ==============================================
+
+import unittest
+import math
+
+from 
