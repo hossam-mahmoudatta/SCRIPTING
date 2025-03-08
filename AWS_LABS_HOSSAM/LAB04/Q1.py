af63c5b480be0c693385d3ead7a14cf2cbb5b2e1
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-03-08 07:05:46 +0200

    added soemthing

--- AWS_LABS_HOSSAM/LAB04/Q1.py
+++ AWS_LABS_HOSSAM/LAB04/Q1.py
@@ -5,13 +5,27 @@ import json
 s3 = boto3.client("s3")
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("dynamoDB-HosaLambda")
+destination_bucket = "hossam-destinationbucket" 
 
 def lambda_handler(event, context):
     for record in event["Records"]:
-        bucket = record["s3"]["bucket"]["name"]
+        source_bucket = record["s3"]["bucket"]["name"]
         file_name = record["s3"]["object"]["key"]
         
-        table.put_item(Item={"file_name": file_name, "bucket": bucket})
-        print(f"File {file_name} added to DynamoDB from {bucket}")
+        # Copy file to destination bucket
+        s3.copy_object(
+            Bucket=destination_bucket,
+            Key=file_name,
+            CopySource= {"Bucket": source_bucket, "Key": file_name},
+        )
+        
+        table.put_item(
+            Item = {
+                "file_name": file_name,
+                "source_bucket": source_bucket,
+                "destination_bucket": destination_bucket
+                }
+        )
+        print(f"File {file_name} added to DynamoDB from {source_bucket}")
     
     return {"StatusCode": 200, "body": json.dumps("Success")}
